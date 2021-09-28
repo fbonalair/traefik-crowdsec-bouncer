01c6cdabbf2e54f68be113adc4558fee5bdf53d6
Fabien Bonalair <bonalair.f@sfeir.com>
2021-09-29 00:12:07 +0200

    [BOUNCER] WIP: implementing main flow

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # traefik-crowdsec-bouncer
 A http service to verify request according to decisions made by CrowdSec.
+
+# Contribution
+
+## Installation
+# TODO
+- go get
+- docker-compose up
+- docker-compose exec crowdsec cscli bouncers add MyTestClient
+  - save API KEY
+  - Add key to server env var CROWDSEC_BOUNCER_API_KEY 
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
+
+type Decision struct {
+	Id        int    `json:"id"`
+	Origin    string `json:"origin"`
+	Type      string `json:"type"`
+	Scope     string `json:"scope"`
+	Value     string `json:"value"`
+	Duration  string `json:"duration"`
+	Scenario  string `json:"scenario"`
+	Simulated bool   `json:"simulated"`
+}
 
 func main() {
 	// TODO: Verifying env vars
@@ -23,10 +38,53 @@ func ping(c *gin.Context) {
 }
 
 func forwardAuth(c *gin.Context) {
+	// TODO in case of error, default reject request
 	println("Authorising request") // FIXME debug
 
-	// TODO get headers
-	// TODO call crowsec API
-	// TODO business logic
-	c.Status(http.StatusOK)
+	// get headers
+	realIP := c.Request.Header.Get("X-Real-Ip") // TODO sanitized ip
+	//forwardedFor = c.Request.Header.Get("X-Forwarded-For")
+
+	// Call crowsec API
+	tr := &http.Transport{
+		MaxIdleConns:    10,
+		IdleConnTimeout: 30 * time.Second,
+	}
+	client := &http.Client{Transport: tr}
+
+	// TODO use Url instead of string
+	//decisionApiUrl := url.URL{
+	//	Host: "localhost",
+	//	Path: "/v1/decisions",
+	//}
+	//decisionApiUrl := "http://localhost:8083/v1/decisions?ip="
+	decisionApiUrl := fmt.Sprintf("http://localhost:8083/v1/decisions?ip=%s", realIP)
+
+	req, err := http.NewRequest(http.MethodGet, decisionApiUrl, nil)
+	if err != nil {
+		fmt.Printf("error %s", err)
+		return
+	}
+	req.Header.Add("X-Api-Key", `40796d93c2958f9e58345514e67740e5`) // TODO extract from env var
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("error %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	reqBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("error %s", err)
+		return
+	}
+
+	var decisions []Decision
+	json.Unmarshal(reqBody, &decisions)
+
+	// Authorization logic
+	if len(decisions) > 0 {
+		c.Status(http.StatusUnauthorized)
+	} else {
+		c.Status(http.StatusOK)
+	}
 }
