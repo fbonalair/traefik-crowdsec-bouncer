da02ccf505c9438fe9c275628c6b732283acf1f8
Fabien Bonalair <bonalair.f@sfeir.com>
2021-09-28 20:27:57 +0200

    [COMPOSE] Implementing traefik forwardAuth middleware

--- main.go
+++ main.go
@@ -6,16 +6,27 @@ import (
 )
 
 func main() {
+	// TODO: Verifying env vars
 	router := gin.Default()
-	router.GET("/api/v1/ping", Ping)
+	router.GET("/api/v1/ping", ping)
+	router.GET("/api/v1/forwardAuth", forwardAuth)
 	err := router.Run()
 	if err != nil {
-		println("An error occurred while starting server", err)
+		println("An error occurred while starting server: ", err)
 		return
 	}
 
 }
 
-func Ping(c *gin.Context) {
+func ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
+}
+
+func forwardAuth(c *gin.Context) {
+	println("Authorising request") // FIXME debug
+
+	// TODO get headers
+	// TODO call crowsec API
+	// TODO business logic
+	c.Status(http.StatusOK)
 }
