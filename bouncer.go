7285ccf8ccb49eeeb73e291ff5e302e657e4fbbe
Fabien Bonalair <bonalair.f@sfeir.com>
2021-09-29 16:22:22 +0200

    [TEST] adding some test

--- bouncer.go
+++ bouncer.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/api/v1/ping", Ping)
-	router.GET("/api/v1/forwardAuth", ForwardAuth)
+	router := setupRouter()
 	err := router.Run()
 	if err != nil {
 		println("An error occurred while starting bouncer: ", err)
 		return
 	}
 
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/api/v1/ping", Ping)
+	router.GET("/api/v1/forwardAuth", ForwardAuth)
+	return router
 }
