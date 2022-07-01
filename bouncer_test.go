66680bf177fe57754a0a2098bf39b25504790ca0
Fabien BONALAIR <f.bonalair@sfeir.com>
2022-07-01 16:12:20 +0200

    feat(ban_code): WIP tests

--- bouncer_test.go
+++ bouncer_test.go
@@ -27,6 +27,17 @@ func TestHealthz(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 }
+func TestMetrics(t *testing.T) {
+	router, _ := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/metrics", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Contains(t, w.Body.String(), "go_info")
+	assert.Contains(t, w.Body.String(), "crowdsec_traefik_bouncer_processed_ip_total")
+}
 
 func TestForwardAuthInvalidIp(t *testing.T) {
 	router, _ := setupRouter()
@@ -59,14 +70,23 @@ func TestForwardAuthValidIp(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 }
-func TestMetrics(t *testing.T) {
+
+/**
+FIXME Since we are using var in module, they are loaded before tests. So changing their values with environment variables have no effect.
+*/
+func testForwardAuthBannedIpCustomResponse(t *testing.T) {
+	// Setup
+	expectedResponseMsg := "Not Found"
+	t.Setenv("CROWDSEC_BOUNCER_BAN_RESPONSE_CODE", "404")
+	t.Setenv("CROWDSEC_BOUNCER_BAN_RESPONSE_MSG", expectedResponseMsg)
+
 	router, _ := setupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/v1/metrics", nil)
+	req, _ := http.NewRequest("GET", "/api/v1/forwardAuth", nil)
+	req.RemoteAddr = "1.2.3.4:48328"
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
-	assert.Contains(t, w.Body.String(), "go_info")
-	assert.Contains(t, w.Body.String(), "crowdsec_traefik_bouncer_processed_ip_total")
+	assert.Equal(t, 404, w.Code)
+	assert.Equal(t, expectedResponseMsg, w.Body.String())
 }
